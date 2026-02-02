618aae7d7f0d1cdeaa5d212a75565461e5573b50
swapno mondol <swapno963@gmail.com>
2026-02-02 16:20:26 +0600

    explored slice len and cap

--- arrar_slice/arrar_slice.go
+++ arrar_slice/arrar_slice.go
@@ -13,4 +13,14 @@ func main() {
 	//  You’re modeling something physical and bounded, like a 3D vector or a fixed-size hash block
 
 	fmt.Println(a, b)
+
+	// Slice: len vs cap
+
+	// len: how many elements you can safely access
+	// cap: how many elements you can grow into before reallocation
+
+	nums := []int{1, 2, 3}
+	fmt.Println(len(nums)) // visible elements
+	fmt.Println(cap(nums)) // total available space
+
 }
