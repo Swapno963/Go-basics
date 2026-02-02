9b86724fd374d298485ab33de878cd3597a1b72c
swapno mondol <swapno963@gmail.com>
2026-02-02 16:15:29 +0600

    explored when to use array

--- arrar_slice/arrar_slice.go
+++ arrar_slice/arrar_slice.go
@@ -6,5 +6,11 @@ func main() {
 	// Array: Fixed size, Size is part of the type, Rarely used directly
 	var a [3]int
 	var b [4]int // different type
+
+	// Arrays shine when:
+	//  The size is truly fixed and small
+	//  You want value semantics (copy on pass)
+	//  You’re modeling something physical and bounded, like a 3D vector or a fixed-size hash block
+
 	fmt.Println(a, b)
 }
