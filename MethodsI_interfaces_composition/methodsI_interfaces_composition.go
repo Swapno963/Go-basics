44268c39ecdb58a47eb7ea5adcb5db4218fd0d0a
swapno mondol <swapno963@gmail.com>
2026-02-03 15:27:48 +0600

    Send by value, for that name was not updated

--- MethodsI_interfaces_composition/methodsI_interfaces_composition.go
+++ MethodsI_interfaces_composition/methodsI_interfaces_composition.go
@@ -10,7 +10,15 @@ func (u User) Greet() string {
 	return "Hello, " + u.Name
 }
 
+// Value Received
+func (u User) Rename(name string) {
+	u.Name = name
+}
+
 func main() {
 	u := User{Name: "Swapno"}
+	fmt.Println(u.Greet())
+
+	u.Rename("Rokon")
 	fmt.Println(u.Greet())
 }
