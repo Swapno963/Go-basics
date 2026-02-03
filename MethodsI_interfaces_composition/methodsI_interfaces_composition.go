6edd8774fce38dd5fd6a7a78deb82a4ad33a8474
swapno mondol <swapno963@gmail.com>
2026-02-03 15:40:19 +0600

    Added the difference of when to use what

--- MethodsI_interfaces_composition/methodsI_interfaces_composition.go
+++ MethodsI_interfaces_composition/methodsI_interfaces_composition.go
@@ -10,8 +10,13 @@ func (u User) Greet() string {
 	return "Hello, " + u.Name
 }
 
-// Value Received
+// Value Received: The method is read-only, The struct is small and immutable
 func (u User) Rename(name string) {
+	u.Name = name
+}
+
+// Pointer received: to modify the struct, The struct is large, want consistent behavior
+func (u *User) RenamePointer(name string) {
 	u.Name = name
 }
 
@@ -20,5 +25,8 @@ func main() {
 	fmt.Println(u.Greet())
 
 	u.Rename("Rokon")
+	fmt.Println(u.Greet())
+
+	u.RenamePointer("shek")
 	fmt.Println(u.Greet())
 }
